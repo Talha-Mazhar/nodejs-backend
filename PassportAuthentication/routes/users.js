be61344e69cfe76483c9136c6eac9a97f8623090
talha-deutics <talha.mazhar@deutics.com>
2023-12-25 02:44:12 +0500

    form fields are submitted and extracted through req.body

--- PassportAuthentication/routes/users.js
+++ PassportAuthentication/routes/users.js
@@ -9,4 +9,10 @@ router.get('/login', (req, res) => res.render('login'))
 
 router.get('/register', (req, res) => res.render('register'))
 
+//Register Handle
+router.post('/register', (req, res) => {
+    const { name, email, password, password2 } = req.body
+    //validation
+})
+
 module.exports = router
