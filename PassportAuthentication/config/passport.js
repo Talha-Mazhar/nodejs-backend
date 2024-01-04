acf760f07061b5fd8c455b3643e6af8c4c57d541
Talha-Mazhar <ahmadnutt9111@gmail.com>
2024-01-04 12:23:53 +0500

    validate email and pasword wioth local stragstrtegy

---
+++ PassportAuthentication/config/passport.js
@@ -0,0 +1,40 @@
+const LocalStrategy = require('passport-local').Strategy
+const mongoose = require('mongoose') //here login is used so mongoose
+const bcrypt = require('bcryptjs') //to hash password to decrypt password
+
+//Local User Modal
+const User = require('../modals/User')
+module.exports = function (passport) {
+    passport.use(
+        new LocalStrategy(
+            { usernameField: 'email' },
+            (email, password, done) => {
+                //Match User
+                User.findOne({ email: email })
+                    .then(user => {
+                        if (!user) {
+                            return done(null, false, {
+                                message: 'That email is not registered',
+                            })
+                        }
+                        //Match Password
+                        bcrypt.compare(
+                            password,
+                            user.password,
+                            (err, isMatch) => {
+                                if (err) throw err
+                                if (isMatch) {
+                                    return done(null, user)
+                                } else {
+                                    return done(null, false, {
+                                        message: 'Password Incorrect',
+                                    })
+                                }
+                            }
+                        )
+                    })
+                    .catch(err => console.log(err))
+            }
+        )
+    )
+}
