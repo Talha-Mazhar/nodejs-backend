2ea8dd4d0b912a5cb7f7c1abcfb03bd6332a3fa0
Talha-Mazhar <ahmadnutt9111@gmail.com>
2024-01-04 12:02:05 +0500

    flash messages

--- PassportAuthentication/app.js
+++ PassportAuthentication/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 const app = express()
 
@@ -13,8 +15,26 @@ mongoose
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err))
 
-//MiddleWare
-//Ejs
+//MiddleWare For Express Session
+app.use(
+    session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true,
+    })
+)
+
+//MiddleWare for connect flash
+app.use(flash())
+
+//Global Vars
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    next()
+})
+
+//MiddleWare Ejs
 app.use(expressLayouts)
 app.set('view engine', 'ejs')
 
