a4578ad5e757cfe20add0a72e0d5fca14123abf5
Talha-Mazhar <ahmadnutt9111@gmail.com>
2024-01-07 14:01:02 +0500

    jwt authentication added

--- PassportAuthentication/routes/index.js
+++ PassportAuthentication/routes/index.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const router = express.Router()
 const { ensureAuthenticated } = require('../config/auth')
-
+const { ensureToken } = require('../config/accessTokens')
+const jwt = require('jsonwebtoken')
 //Welcome Page
 router.get('/', (req, res) => res.render('welcome'))
 
 //Dashboard
-router.get('/dashboard', ensureAuthenticated, (req, res) =>
-    res.render('dashboard', {
-        name: req.user.name,
+router.get('/dashboard', ensureToken, ensureAuthenticated, (req, res) => {
+    jwt.verify(req.access_token, 'secretkey', (err, authData) => {
+        if (err) {
+            res.sendStatus(403)
+        } else {
+            res.render('dashboard', {
+                name: req.user.name,
+            })
+            res.json({
+                authData,
+            })
+        }
     })
-)
+})
 
 module.exports = router
